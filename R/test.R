306db8340383184257b9ddbf26ff58296a70b2bf
decampcl <decampcl@wificampus-073117.grenet.fr>
2019-03-27 15:18:33 +0100

    New package ≠ôπ

---
+++ R/test.R
@@ -0,0 +1,12 @@
+
+#' Test function
+#' 
+#'This function print a test
+#' @return this function print "this is a test"
+#' @export
+#'
+#' @examples
+#' test()
+test = function() {
+  print("this is a test")
+}
